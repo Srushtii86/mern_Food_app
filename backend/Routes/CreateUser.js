be1a555b6501c93ce610b2cd2ac10104132cdeeb
abhishekb740 <abhishekb740@gmail.com>
2023-09-25 18:04:10 +0530

    Changes

--- backend/Routes/CreateUser.js
+++ backend/Routes/CreateUser.js
@@ -1,33 +1,37 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const{body,validationResult} = require('express-validator');
+const { body, validationResult } = require("express-validator");
 
-const User = require('../models/user')
-router.post("/CreateUser",
-[
-    body('name','Invalid Name').isLength({min:3}),
-    body('email','Invalid Email').isEmail(),
-    body('password','Invalid Password').isLength({min:5}),
-]
-,async(req,res)=>{
+const User = require("../models/user");
+router.post(
+  "/CreateUser",
+  [
+    body("name", "Invalid Name").isLength({ min: 3 }),
+    body("email", "Invalid Email").isEmail(),
+    body("password", "Invalid Password").isLength({ min: 5 }),
+  ],
+  async (req, res) => {
+    console.log(req.body);
     const errors = validationResult(req);
-    console.log("Hello");
-    if(!errors.isEmpty()){
-        return res.status(400).json({errors:errors.array()});
+    console.log(errors);
+    console.log("Hello5655");
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-    try{
-        await User.create({
-            name:req.body.name,
-            password:req.body.password,
-            email:req.body.email,
-            location:req.body.location
-        })
-        res.json({success:true})
+    try {
+      await User.create({
+        name: req.body.name,
+        password: req.body.password,
+        email: req.body.email,
+        location: req.body.location,
+      });
+      console.log("Data Saved Successfully");
+      res.json({ success: true });
+    } catch (error) {
+      console.log(error);
+      res.json({ success: false });
     }
-    catch(error){
-        console.log(error)
-        res.json({success:false})
-    }
-})
+  }
+);
 
 module.exports = router;
--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,17 @@
 const express = require('express')
 const app = express()
-const port = 5000
+const port = 5000;
 const connectToMongoDB= require("./db.js");
+const cors = require("cors");
 connectToMongoDB();
 
-app.use((req,res,next)=>{
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header("Access-Control-Allow-Headers","Origin,X-Requested-With,Content-Type,Accept");
-  next();
-})
+
+app.use(cors());
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  console.log("Hello World");
 })
 
 app.use('/api' , require("./Routes/CreateUser.js"));
